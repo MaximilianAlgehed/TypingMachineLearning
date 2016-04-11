7b09e496c29c4775c3af3944e6a1e2565edbc29b
Maximilian Algehed <m.algehed@gmail.com>
2016-04-11 21:49:36 +0200

    Added general gradient descent algorithm and added sum of the square error cost function. It works

--- LinearRegression.hs
+++ LinearRegression.hs
@@ -21,7 +21,7 @@ type TrainingResults a = Matrix a
 type TrainingSet a = (TrainingData a, TrainingResults a)
 
 -- The cost function, J
-type CostFunction a = TrainingSet a -> Parameters a -> a
+type CostFunction a = Hypothesis a -> TrainingSet a -> Parameters a -> DM a
 
 -- The del operator
 del :: (Num a) => (Matrix a -> DM a) -> Matrix a -> Matrix a
@@ -34,3 +34,16 @@ linearHypothesis xs = value
         value = foldl1 (+) [(idm 1 n (1, i)) * (cons 1 n (xs'!(1, i))) | i <- [1..n]]
         n     = ncols xs'
         xs'   = matrix 1 1 (const 1) <|> xs
+
+-- Square error cost function
+squareError :: (Num a) => CostFunction a
+squareError h (d, r) = value
+    where
+        value = foldl1 (+) [let x = (h (rowVector (getRow i d))) - (cons 1 n (r!(i, 1))) in x*x | i <- [1..n]]
+        n     = nrows r
+
+-- Gradient descent algorithm
+gradientDescent :: (Num a) => a -> Hypothesis a -> CostFunction a -> TrainingSet a -> Parameters a -> [Parameters a]
+gradientDescent alpha h j s theta_0 = iterate descend theta_0
+    where
+        descend theta = theta - (fmap (alpha*) ((del (j h s)) theta))
